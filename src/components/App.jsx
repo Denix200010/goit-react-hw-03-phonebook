8628dba240352ed4ac4b5c14507bd9656e383740
Denys Ostrianskyi <103590306+Denix200010@users.noreply.github.com>
2022-11-06 10:59:16 +0200

    added local storage

---
+++ src/components/App.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+
+import { nanoid } from 'nanoid';
+
+import style from './app.module.css';
+
+import { PhonebookList } from "./PhonebookList/PhonebookList";
+import { ContactsForm } from "./ContactsForm/ContactsForm";
+import { FilterInput } from "./FilterForm/FilterInput";
+
+const CONTACTS_KEY = 'CONTACTS_KEY';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  }
+  
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem(CONTACTS_KEY));
+    savedContacts && this.setState(({ contacts: savedContacts }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(CONTACTS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    let alreadyContact = null;
+    const form = e.currentTarget;
+    const name = form.elements.name.value;
+    this.state.contacts.forEach(contact => {
+      if (contact.name.toLowerCase() === name.toLowerCase()) {
+        alreadyContact = true;
+      }
+    })
+    if (alreadyContact) {
+        return alert(`${name} is already in contacts`);
+      }
+    const number = form.elements.number.value;
+    const id = nanoid();
+    const newContact = {
+      id: id,
+      name: name,
+      number: number,
+    }
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts]
+    }))
+  }
+  onFilterInput = (e) => {
+    this.setState({ filter: e.currentTarget.value } )
+  }
+  getSearchContact = () => {
+    const normalizedSearchContacts = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedSearchContacts))
+  }
+  onDeleteContact = (id) => {
+    this.setState(prevState => ({
+      ...prevState,
+      contacts: prevState.contacts.filter(contact => contact.id !== id)
+    }))
+  }
+  render() {
+    const actuallyContacts = this.getSearchContact();
+    return <div className={style.container}>
+      <h1>Phonebook</h1>
+      <ContactsForm
+        onSubmitFunction={this.onFormSubmit} />
+
+      <FilterInput
+        onFilterInput={this.onFilterInput} />
+      <h2>Contacts</h2>
+      <PhonebookList
+        contact={actuallyContacts}
+        onDeleteContact={this.onDeleteContact} />
+      </div>
+  }
+};
